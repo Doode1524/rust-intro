fda91c9fa994aba43c5b3139e35f7f7eb1c00bf4
joey <doode1524@gmail.com>
2022-03-01 20:50:42 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    use std::io;
+    println!("What is your name?");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Failed to read line");
+    println!("Hello {}!", name.trim());
+}
